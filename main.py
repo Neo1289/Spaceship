3399ae5a1415769a46dfc2f9a61345a34f5245e4
Neo1289 <127672351+Neo1289@users.noreply.github.com>
2024-08-04 10:10:13 +0200

    Update main.py

--- main.py
+++ main.py
@@ -66,11 +66,9 @@ testY = 10
 # Game Over
 over_font = pygame.font.Font('freesansbold.ttf', 64)
 
-
 def show_score(x, y):
     score = font.render("Score : " + str(score_value), True, (0, 255, 0))
     screen.blit(score, (x, y))
-
 
 def game_over_text():
     over_text = over_font.render("GAME OVER", True, (0, 255, 0))
@@ -80,16 +78,13 @@ def game_over_text():
 def player(x, y):
     screen.blit(playerImg, (x, y))
 
-
 def enemy(x, y, i):
     screen.blit(enemyImg[i], (x, y))
-
 
 def fire_bullet(x, y):
     global bullet_state
     bullet_state = "fire"
     screen.blit(bulletImg, (x + 16, y + 10))
-
 
 def isCollision(enemyX, enemyY, bulletX, bulletY):
     distance = math.sqrt(math.pow(enemyX - bulletX, 2) + (math.pow(enemyY - bulletY, 2)))
@@ -97,7 +92,6 @@ def isCollision(enemyX, enemyY, bulletX, bulletY):
         return True
     else:
         return False
-
 
 # Game Loop
 running = True
